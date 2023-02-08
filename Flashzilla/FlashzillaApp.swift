f34dc9a3c799dd11c505151ce4aa557ca73cd26f
Konneki <konnekdaniel@gmail.com>
2023-02-08 20:21:17 +0100

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Daniel Konnek on 8.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
