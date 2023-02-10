a59c3154c99315e1fff130555371de0be3678ca2
Konneki <konnekdaniel@gmail.com>
2023-02-10 22:16:08 +0100

    Added Card class and created CardView. Limited the orientation to landscape only

---
+++ Flashzilla/Card.swift
@@ -0,0 +1,15 @@
+//
+//  Card.swift
+//  Flashzilla
+//
+//  Created by Daniel Konnek on 10.02.2023.
+//
+
+import Foundation
+
+struct Card {
+    let prompt: String
+    let answer: String
+    
+    static let example = Card(prompt: "Who played the 13th doctor in Doctor Who?", answer: "Jodie Whittaker")
+}
